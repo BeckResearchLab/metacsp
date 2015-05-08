52c91e9a1145f2d7940ed6c55aab523ecf132ec7
Dave Beck <dacb@uw.edu>
2015-05-07 22:10:43 -0700

    refinements of formatting

--- phylodist/test.py
+++ phylodist/test.py
@@ -2,25 +2,21 @@ import unittest
 
 import io
 
-class TestIO(unittest.TestCase):
+class test_LoadFile(unittest.TestCase):
 	
 	def test_loadFile_filenameType(self):
-		# test non-string filename raises an error
 		with self.assertRaises(TypeError):
 			io.loadFile(42)
 
 	def test_loadFile_verboseType(self):
-		# test non-bool verbose raises an error
 		with self.assertRaises(TypeError):
 			io.loadFile("filename", 42)
 
 	def test_loadFile_nonExistentFile(self):
-		# test a non-existent file raises an error
 		with self.assertRaises(IOError):
 			io.loadFile("file_does_not_exist")
 
 	def test_loadFile_directoryNotFile(self):
-		# test a directory instead of file raises an error
 		with self.assertRaises(IOError):
 			io.loadFile("examples")
 
