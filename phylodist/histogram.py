b3105069cfd29b0ac72b28854808c9684e442639
Dave Beck <dacb@uw.edu>
2015-05-08 11:15:08 -0700

    refactoring and added multilevel merge

--- phylodist/histogram.py
+++ phylodist/histogram.py
@@ -30,7 +30,7 @@ def computeAll(phylodistDataFrame):
 		# next, append the percent column
 		taxHistDict[taxLevel]['percent'] = taxHistDict[taxLevel].apply(lambda x: 100*x/float(x.sum()))
 
-	return(taxHistDict)
+	return taxHistDict
 
 def computeAllForSamples(phylodistSweepDict):
 	"""Create histograms of occurences at taxonomy levels for
@@ -53,11 +53,49 @@ def computeAllForSamples(phylodistSweepDict):
 		sampleDictTaxHistDict[key] = computeAll(phylodistSweepDict[key])
 	print("done")
 
-	return(sampleDictTaxHistDict)
+	return sampleDictTaxHistDict
 
-def plotForSamples(sampleDictTaxHistDict, taxonomyLevel):
-	"""Create a stacked bar chart for the taxonomy abundance percentages
-		for a specific taxonomic level
+def validateSampleDictTaxHistDict(sampleDictTaxHistDict):
+	"""Sanity check a sampleDictTaxHistDict
+		Primarily used by merge functions
+
+	Args:
+		sampleDictTaxHistDict (dict): dictionary of phylodistDataFrames for
+			all samples
+	
+	Returns:
+		nothing, raises errors on sanity failures
+	"""
+	if not isinstance(sampleDictTaxHistDict, dict):
+		raise TypeError("argument sampleDictTaxHistDict should be a dict")
+	samples = len(sampleDictTaxHistDict)
+	if samples < 1:
+		raise ValueError("argument sampleDictTaxHistDict must have at least one entry")
+
+def mergeAcrossSamplesTaxLevels(sampleDictTaxHistDict):
+	"""Create a dictionary of data frames that are merged at taxonomy
+		levels.  The key into this dictionary is the taxonomy level.
+
+	Args:
+		sampleDictTaxHistDict (dict): dictionary of phylodistDataFrames for
+			all samples
+
+	Returns:
+		a new dictionary with keys for eah taxonomy level containing
+			a merged ataframe with histograms from all samples
+	"""
+	validateSampleDictTaxHistDict(sampleDictTaxHistDict)
+
+	taxonomyDictTaxHist = {}
+	for taxonomyLevel in TAXONOMY_HIERARCHY:
+		taxonomyDictTaxHist[taxonomyLevel] = mergeAcrossSamples(sampleDictTaxHistDict, taxonomyLevel)
+
+	return taxonomyDictTaxHist
+
+
+def mergeAcrossSamples(sampleDictTaxHistDict, taxonomyLevel):
+	"""Create a single data frame by merging all samples for a given
+		taxonomy level
 
 	Args:
 		sampleDictTaxHistDict (dict): dictionary of phylodistDataFrames for
@@ -65,19 +103,16 @@ def plotForSamples(sampleDictTaxHistDict, taxonomyLevel):
 		taxonomyLevel (str): taxonomy level to use in chart
 
 	Returns:
-		? figure or nothing ? undecided
+		a single merged data frame with all of the samples' data
 	"""
-	
-	if (not taxonomyLevel in TAXONOMY_HIERARCHY):
-		raise ValueError
+	if not taxonomyLevel in TAXONOMY_HIERARCHY:
+		raise ValueError("taxonomy level ' + taxonomyLevel + ' is not recognized")
 
-	samples = len(sampleDictTaxHistDict)
-	if (samples < 1):
-		raise ValueError
+	validateSampleDictTaxHistDict(sampleDictTaxHistDict)
 
 	first = True
 	for sample in sampleDictTaxHistDict.keys():
-		if (first):
+		if first:
 			tmpDF = sampleDictTaxHistDict[sample][taxonomyLevel].drop('count', 1)
 			mergedPhylodistHist = tmpDF.rename(
 					columns = { 'percent' : sample }
@@ -93,6 +128,22 @@ def plotForSamples(sampleDictTaxHistDict, taxonomyLevel):
 				)
 		first = False
 
+	return mergedPhylodistHist
+
+
+def plotForSamples(mergedPhylodistHist):
+	"""Create a stacked bar chart for the taxonomy abundance percentages
+		from a merged phylodist histogram
+
+	Args:
+		mergedPhylodistHist (data frame): single table with taxonomy as levels
+			and samples as columns
+			all samples
+		taxonomyLevel (str): taxonomy level to use in chart
+
+	Returns:
+		? figure or nothing ? undecided
+	"""
 	plot = mergedPhylodistHist.T.plot(kind='bar', stacked=True, legend=False)
 	fig = plot.get_figure()
 	fig.savefig("output.png")
