b35c5809457164363cd06e81d42b12428ce5ce6b
Dave Beck <dacb@uw.edu>
2015-05-13 09:48:04 -0700

    updated with more testing examples

--- example.py
+++ example.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python
 
-import pandas as pd
+import pip
+
+import metadata.io
 
 import phylodist.io
 import phylodist.histogram
 
-import metadata.io
+DATA_ROOT = 'examples/valid'
 
-mdDF = metadata.io.loadFile('examples/metadata.tab', indexCol=0, verbose=True)
+metadataDF = metadata.io.loadFile(
+    DATA_ROOT + '/metadata.tab', indexCol=0, verbose=True
+    )
 
 phylos = phylodist.io.sweepFiles(
-    "/dacb/globus",
-    sampleNameExtractionFunction=metadata.io.defaultSampleNameExtractionFunction
+    DATA_ROOT
     )
-#phylos = phylodist.io.sweepFiles("examples/valid")
+
 sampleDictTaxHistDict = phylodist.histogram.computeAllForSamples(phylos)
 taxonomyDictTaxHist = phylodist.histogram.mergeAcrossSamplesTaxLevels(sampleDictTaxHistDict)
 
-#phylodist.histogram.plotForSamples(taxonomyDictTaxHist['class'])
+print(sampleDictTaxHistDict['exampleSample']['kingdom'])
 
-writer = pd.ExcelWriter('output.xlsx')
-for taxonomyLevel in taxonomyDictTaxHist.keys():
-    taxonomyDictTaxHist[taxonomyLevel].to_excel(writer, taxonomyLevel)
-writer.save()
+phylodist.histogram.plotForSamples(taxonomyDictTaxHist['class'])
 
-#print(sampleDictTaxHistDict['exampleSample']['kingdom'])
-#print(sampleDictTaxHistDict['exampleSample']['phylum'])
-
-#print(taxHistograms['kingdom'])
-#print(taxHistograms['phylum'])
-#print(taxHistograms['class'])
-#print(taxHistograms['order'])
-#print(taxHistograms['family'])
-
-#import pip
-#with open("requirements.txt", "w") as f:
-#    for dist in pip.get_installed_distributions():
-#        req = dist.as_requirement()
-#        f.write(str(req) + "\n")
+with open("requirements.txt", "w") as f:
+    for dist in pip.get_installed_distributions():
+        req = dist.as_requirement()
+        f.write(str(req) + "\n")
