c0e7b425f7e51c97f5e44aa42f4bcc55a78caa85
Dave Beck <dacb@uw.edu>
2015-05-12 23:20:58 -0700

    check point of sources

--- example.py
+++ example.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
 
+import pandas as pd
+
 import phylodist.io
 import phylodist.histogram
 
-phylos = phylodist.io.sweepFiles("/dacb/globus")
+import metadata.io
+
+mdDF = metadata.io.loadFile('examples/metadata.tab', indexCol=0, verbose=True)
+
+phylos = phylodist.io.sweepFiles(
+    "/dacb/globus",
+    sampleNameExtractionFunction=metadata.io.defaultSampleNameExtractionFunction
+    )
 #phylos = phylodist.io.sweepFiles("examples/valid")
 sampleDictTaxHistDict = phylodist.histogram.computeAllForSamples(phylos)
 taxonomyDictTaxHist = phylodist.histogram.mergeAcrossSamplesTaxLevels(sampleDictTaxHistDict)
 
 #phylodist.histogram.plotForSamples(taxonomyDictTaxHist['class'])
-
-import pandas as pd
 
 writer = pd.ExcelWriter('output.xlsx')
 for taxonomyLevel in taxonomyDictTaxHist.keys():
@@ -26,8 +33,8 @@ writer.save()
 #print(taxHistograms['order'])
 #print(taxHistograms['family'])
 
-import pip
-with open("requirements.txt", "w") as f:
-    for dist in pip.get_installed_distributions():
-        req = dist.as_requirement()
-        f.write(str(req) + "\n")
+#import pip
+#with open("requirements.txt", "w") as f:
+#    for dist in pip.get_installed_distributions():
+#        req = dist.as_requirement()
+#        f.write(str(req) + "\n")
